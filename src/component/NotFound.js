53d0aa8abb5bf209eb1b553fd7728a8854d93c8d
Maxime Molinillo <max_224@hotmail.fr>
2022-03-12 11:00:12 +0100

    refonte projet

--- src/component/NotFound.js
+++ src/component/NotFound.js
@@ -1,8 +1,15 @@
 
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Header from './Header';
 
 const NotFound = () =>
-
-    <h1>Erreur 404 page non touvée</h1>
-
+    <div className='smallcontainer'>
+          
+        <h2 className='error'>Erreur 404 page non touvée</h2>
+        <header className="App-header">
+                <Header />
+            </header>
+     <Link  className='sC' to={"/"}>Retour à l'accueil</Link>
+    </div>
 export default NotFound
---
+++ src/technicien.js
@@ -0,0 +1,24 @@
+const techniciens = [
+    {
+        id: 1,
+        nom: "Jean-Louis Lecca"
+    },
+    {
+        id: 2,
+        nom: "Jean-Michel Larqué"
+    },
+    {
+        id: 3,
+        nom: "Jean-Francis Laplage"
+    },
+    {
+        id: 4,
+        nom: "Jean-Francois Copé"
+    },
+    {
+        id: 5,
+        nom: "Jean-Franck Mickael"
+    }
+];
+
+export default techniciens;
